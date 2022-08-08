05dd752a21a9481217193b1a404215cd695e15b4
Manuel Avelar <me@mavelar.com>
2022-08-08 15:20:30 -0500

    Module name changed

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'DUna_Payments',
+    'DEUNA_Checkout',
     __DIR__
 );
